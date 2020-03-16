4e1eb0b1710dfaf94d4a05557ebcb0028a578014
BenjaminElifLarsen <62103451+BenjaminElifLarsen@users.noreply.github.com>
2020-03-16 09:54:55 +0100

    Add files via upload

---
+++ GuessANumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GuesssANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+
+    public class Interface
+    {
+
+    }
+
+    
+
+}
