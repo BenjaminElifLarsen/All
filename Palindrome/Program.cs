a4ce6e11e4578f0bee55f6438e7a8a8505dde70d
BenjaminElifLarsen <62103451+BenjaminElifLarsen@users.noreply.github.com>
2020-03-16 09:32:43 +0100

    Add files via upload

---
+++ Palindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(PalindromeChecker("boob"));
+            Console.WriteLine(PalindromeChecker("Boob"));
+            Console.WriteLine(PalindromeChecker("Check"));
+            Console.WriteLine(PalindromeChecker("check"));
+            Console.WriteLine(PalindromeChecker("reviver"));
+            Console.WriteLine(PalindromeChecker("Reviver"));
+            Console.ReadLine();
+        }
+
+
+
+        static bool PalindromeChecker(string str)
+        {
+            str = str.ToLower();
+            string reverseStr = "";
+            char[] chars = str.ToCharArray();
+            for (int i = str.Length-1; i >= 0; i--)
+                reverseStr += chars[i];
+            if (reverseStr == str)
+                return true;
+            else
+                return false;
+        }
+
+    }
+
+
+}
+
