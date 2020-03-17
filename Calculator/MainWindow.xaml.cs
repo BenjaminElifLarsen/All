66f81f1466a693c4c2ab9289dfc62bb0e85506ac
BenjaminElifLarsen <62103451+BenjaminElifLarsen@users.noreply.github.com>
2020-03-17 09:35:15 +0100

    Add files via upload
    Early interface and functions for them.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,65 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool plus = false;
+        bool minus = false;
+        bool devide = false;
+        bool multiply = false;
+        string numberText = "";
+        float oldNumber = 0;
+        float newNumber = 0;
+
+
+        private void BoolChanger(bool plus_, bool minus_, bool devide_, bool multiply_)
+        {
+            plus = plus_;
+            minus = minus_;
+            devide = devide_;
+            multiply = multiply_;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private bool GetNumber(out float? num)
+        {
+            string numText = number.Text;
+            number.Text = "";
+            num = 0;
+            float num2;
+            bool isText = Single.TryParse(numberText, out num2) ? true : false;
+            if (isText)
+                num = num2;
+            return isText;
+
+        }
+
+        private void Plus(object sender, RoutedEventArgs e)
+        {
+            BoolChanger(true, false, false, false);
+            
+        }
+
+        private void Minus(object sender, RoutedEventArgs e)
+        {
+            BoolChanger(false, true, false, false);
+        }
+
+        private void Multiply(object sender, RoutedEventArgs e)
+        {
+            BoolChanger(false, false, true, false);
+        }
+
+        private void Devide(object sender, RoutedEventArgs e)
+        {
+            BoolChanger(false, false, false, true);
+        }
+
+        private void Number(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
